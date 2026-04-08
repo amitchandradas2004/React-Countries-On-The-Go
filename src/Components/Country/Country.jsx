ee98aa44cee5aace7be1516cbbbe20a6c4ef21c6
amitchandradas2004 <amitcdas792@gmail.com>
2026-04-08 11:38:28 +0600

    All countries are loaded

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const Country = () => {
+const Country = ({ country }) => {
+  console.log(country);
+
   return (
     <div>
-      
+      <h3>Name: {country.name.common}</h3>
+      <h4>Official Name: {country.name.official}</h4>
+      <h4>Capital: {country.capital.capital}</h4>
+      <h4>Population: {country.population.population}</h4>
+      <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
     </div>
   );
 };
